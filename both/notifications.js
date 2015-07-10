71b48f6c8e1226cb171822afb070a0f6b8f9f6bd
Liam Wooding <liamwooding@gmail.com>
2015-07-10 10:55:48 +0100

    Improved nullchecking in notifications.js

--- both/notifications.js
+++ both/notifications.js
@@ -1,5 +1,5 @@
 Meteor.startup(function() {
-  if (!Meteor.settings.public) return
+  if (!Meteor.settings || !Meteor.settings.public) return
   Meteor.notificationClient = new NotificationClient({
     senderId: Meteor.settings.public.GCM.senderId,
     gcmAuthorization: Meteor.settings.GCM && Meteor.settings.GCM.authorization
